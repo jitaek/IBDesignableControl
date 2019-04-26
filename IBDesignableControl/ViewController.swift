a9df67a9eb6a8eb7083f08b88617898f367095b1
jitae <jitaekim93@gmail.com>
2019-04-26 00:23:07 -0700

    Initial Commit

---
+++ IBDesignableControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBDesignableControl
+//
+//  Created by Jitae Kim on 4/26/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
